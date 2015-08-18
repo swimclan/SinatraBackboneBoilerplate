8bdf2dfba6d1ee9707de46ca5a1ac3fe0261b7e8
Matt Herron <matthew.herron77@gmail.com>
2015-08-18 14:46:41 -0500

    My first boilerplate for BackboneJS boilerplate

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'bundler'
+Bundler.require
+
+  # set folder for templates to ../views, but make the path absolute
+  set :views, File.expand_path('../views', __FILE__)
+  set :public, File.expand_path('../public', __FILE__)
+
+## postgres adapter (TBD)
+
+get '/' do
+	erb :index
+end
